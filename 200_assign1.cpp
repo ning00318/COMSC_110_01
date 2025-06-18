06ec036f7a68e4ac850c7d668bf1cd29995d7125
NINA <ninatsao@ningningning.lan>
2025-06-18 00:26:00 -0700

    6th commit

--- 200_assign1.cpp
+++ 200_assign1.cpp
@@ -13,40 +13,33 @@ int getLowestInRow(int [][COLS], int);
 int main()
 {
 	const int ROWS = 4;  
-	
-	
+		
 	int testArray[ROWS][COLS] =
 					{ { 1,   2,  3,  4,  5 },
 					{   6,   7,  8,  9, 10 },
 					{  11,  12, 13, 14, 15 },
 					{  16,  17, 18, 19, 20 } };
    
-	
 	cout << "The total of the array elements is "
 		 << getTotal(testArray, ROWS)
 		 << endl;
    
-
 	cout << "The average value of an element is "
 		 << getAverage(testArray, ROWS)
 		 << endl;
-
 
 	cout << "The total of row 0 is "
 		 << getRowTotal(testArray, 0)
 		 << endl;
 
-
 	cout << "The total of col 2 is "
 		 << getColumnTotal(testArray, 2, ROWS)
 		 << endl;
 
-     
 	cout << "The highest value in row 2 is "
 		 << getHighestInRow(testArray, 2)
 		 << endl;
 
-//.
 	cout << "The lowest value in row 2 is "
 		 << getLowestInRow(testArray, 2)
 		 << endl;
@@ -56,7 +49,7 @@ int main()
 }
 
 // Comment1: Use 2D array as argument and return the total of values in the array
-int total = 0;
+int total = 0;	// Comment2: Put total values outside for further usage(i.e. find average value)
 int getTotal(int array[][COLS], int rows)
 {
 	for (int i = 0; i < rows; i++) {
@@ -67,15 +60,16 @@ int getTotal(int array[][COLS], int rows)
 	return total;
 }
 
-// Comment2: Use 2D array as argument and return the average of values in the array
+// Comment3: Use 2D array as argument and return the average of values in the array
 double getAverage(int array[][COLS], int rows)
 {
 	double avg;
-    avg = static_cast<double>(total) / (rows * COLS);
+    avg = static_cast<double>(total) / (rows * COLS);	// Comment4: Convert integer to decimal numbers by using static_cast<>()
+														// Comment5: Use the total values we already calculated from getTotal() to find the average
 	return avg;
 }
 
-// Comment3: Use 2D array and specified number of row as arguments, return the total of values in the specified row
+// Comment6: Use 2D array and specified number of row as arguments, return the total of values in the specified row
 int getRowTotal(int array[][COLS], int rowToTotal)
 {
 	int rowTotal = 0;
@@ -84,7 +78,7 @@ int getRowTotal(int array[][COLS], int rowToTotal)
 	return rowTotal;
 }
 
-// Comment4: Use 2D array and specified number of column as arguments, return the total of values in the specified column
+// Comment7: Use 2D array and specified number of column as arguments, return the total of values in the specified column
 int getColumnTotal(int array[][COLS], int colToTotal, int rows)
 {
 	int colTotal = 0;
@@ -93,20 +87,25 @@ int getColumnTotal(int array[][COLS], int colToTotal, int rows)
 	return colTotal;
 }
 
-// Comment5: Use 2D array and specified number of row as arguments, return the maximum value in the specified row
+// Comment8: Use 2D array and specified number of row as arguments, return the maximum value in the specified row
 int getHighestInRow(int array[][COLS], int rowToSearch)
 {
-	int maxInRow = array[rowToSearch][0];
+	int maxInRow = array[rowToSearch][0];		// Comment9: Set column to zero as the initial maximum number
 	for (int j = 1; j < COLS; j++) {
-		if (array[rowToSearch][j] > maxInRow)
-			maxInRow = array[rowToSearch][j];
+		if (array[rowToSearch][j] > maxInRow)	// Comment10: If other colums' number greater than maxInRow, 
+			maxInRow = array[rowToSearch][j];	//			  assign that number to maxInRow instead
 	}
 	return maxInRow;
 }
 
-
+// Comment11: Use 2D array and specified number of row as arguments, return the minimum value in the specified row
 int getLowestInRow(int array[][COLS], int rowToSearch)
 {
-	
+	int minInRow = array[rowToSearch][0];		// Comment12: Set column to zero as the initial minimum number
+	for (int j = 1; j < COLS; j++) {
+		if (array[rowToSearch][j] < minInRow)	// Comment13: If other colums' number less than minInRow, 
+			minInRow = array[rowToSearch][j];	//			  assign that number to minInRow instead
+	}
+	return minInRow;
 }
 
